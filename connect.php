a217b58cea9efb569a63ea817e0c57badadff275
Chalie Wizz <60155767+Chal13W1zz@users.noreply.github.com>
2022-03-06 23:36:25 +0300

    Add default mysql database user

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 //establishing connection with database.
-$conn = new mysqli('localhost','','') or die('Cannot connect to server');
+$conn = new mysqli('localhost','root','') or die('Cannot connect to server');
 $conn->select_db('k_art') or die ('Cannot found database');
 
 ?>
